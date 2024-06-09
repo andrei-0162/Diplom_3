cf287b7280a22c0a684ba39329f6e8e4aa8574ff
student <andrei.hurko@gmail.com>
2024-06-09 20:25:13 +0300

    Yandex - pass

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -7,10 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import userapieditor.Resources;
-
-import java.util.concurrent.TimeUnit;
 
 
 public class DriverFactory extends ExternalResource {
@@ -36,14 +33,11 @@ public class DriverFactory extends ExternalResource {
     private WebDriver driver;
 
     public void initDriver() {
-//        if ("firefox".equals(System.getProperty("browser"))) {
-//            initFirefox();
-//        } else if ("yandex".equals(System.getProperty("browser"))) {
-//            initYa();
-//        } else {
-//            initChrome();
-
-            initYa();
+        if ("yandex".equals(System.getProperty("browser"))) {
+            initYandex();
+        } else {
+            initChrome();
+            }
 
     }
 
@@ -52,32 +46,13 @@ public class DriverFactory extends ExternalResource {
         driver = new ChromeDriver();
     }
 
-    private void initFirefox() {
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
 
-    }
-
-    private void initYa() {
-//        WebDriverManager.chromedriver().setup();
-//        ChromeOptions options = new ChromeOptions();
-//        options.setBinary("C:\\Users\\Xiaomi\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe");
-//        driver = new ChromeDriver(options);
-
-
-
+    private void initYandex() {
             WebDriverManager.chromedriver().setup();
             ChromeOptions options = new ChromeOptions();
+            System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
             options.setBinary("C:\\Users\\Xiaomi\\AppData\\Local\\Yandex\\YandexBrowser\\Application\\browser.exe");
             driver = new ChromeDriver(options);
-
-
-//            options.setBinary(System.getProperty("webdriver.yandex.bin"));
-
-//            options.setCapability("browser", System.getProperty("browser"));
-//            options.setCapability("version", System.getProperty("version"));
-
-//        mvn -Dbrowser=yandex -Dbdriver.version=122.0.6261.94 -Dwebdriver.yandex.bin="C:/Users/Xiaomi/AppData/Local/Yandex/YandexBrowser/Application.browser.exe" test
     }
 
     public WebDriver getDriver() {
--- src/test/java/pageobject/CustomClick.java
+++ src/test/java/pageobject/CustomClick.java
@@ -10,14 +10,19 @@ import java.time.Duration;
 public class CustomClick {
 
     public void clickOnElement(WebDriver driver, By locator) {
-        if ("firefox".equals(System.getProperty("browser"))) {
-            //new WebDriverWait(driver, Duration.ofSeconds(Constants.DEFAULT_TIMER));
-            new JSUtils().clickElementByJS(driver, locator);
-        } else {
-            new WebDriverWait(driver, Duration.ofSeconds(Constants.DEFAULT_TIMER))
-                    .until(ExpectedConditions.elementToBeClickable(locator));
-            driver.findElement(locator).click();
-        }
+//        if ("firefox".equals(System.getProperty("browser"))) {
+//            //new WebDriverWait(driver, Duration.ofSeconds(Constants.DEFAULT_TIMER));
+//            new JSUtils().clickElementByJS(driver, locator);
+//        } else {
+//            new WebDriverWait(driver, Duration.ofSeconds(Constants.DEFAULT_TIMER))
+//                    .until(ExpectedConditions.elementToBeClickable(locator));
+//            driver.findElement(locator).click();
+//        }
 
+
+
+        new WebDriverWait(driver, Duration.ofSeconds(Constants.DEFAULT_TIMER))
+                .until(ExpectedConditions.elementToBeClickable(locator));
+        driver.findElement(locator).click();
     }
 }
